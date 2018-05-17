4fad164acffe18db42edabb72b03abd2654f25fc
frus68783 <frus68783@GFI520007.annuaire.groupe.local>
2018-05-17 11:34:41 +0200

    Refactoring parameters in plugin scm - deletion in parameter.csv - deletion in i18n

--- src/main/resources/META-INF/resources/webjars/service/scm/git/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/git/nls/fr/messages.js
@@ -1,9 +1,5 @@
 define({
 	'service:scm:git:url': 'URL de base',
-	'service:scm:git:repository': 'Dépôt',
-	'service:scm:git:projectname': 'Nom du projet',
-	'service:scm:git:clientname': 'Nom du client',
-	'service:scm:git:ldapgroups': 'Groupes LDAP',
 	'service:scm:git:user': 'Utilisateur',
 	'service:scm:git:password': 'Mot de passe',
 	'error': {
--- src/main/resources/META-INF/resources/webjars/service/scm/git/nls/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/git/nls/messages.js
@@ -2,10 +2,6 @@ define({
 	root: {
 		'service:scm:git': 'Git',
 		'service:scm:git:url': 'Base URL',
-		'service:scm:git:repository': 'Repository',
-		'service:scm:git:projectname': 'Project name',
-		'service:scm:git:clientname': 'Client name',
-		'service:scm:git:ldapgroups': 'LDAP groups',
 		'service:scm:git:user': 'User',
 		'service:scm:git:password': 'Password',
 		'error': {
